6b8601625dac296f6a35851d1ad5e054e5962d12
Your Name <you@example.com>
2025-06-23 14:10:26 +0200

    Uitwerking van de exercise

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('layouts.layoutadmin');
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
