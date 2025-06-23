5bbaf4b91276233eaf1d3fe14d477add68dc48d8
Thijs Vos <53115@hoornbeeck.nl>
2025-06-23 12:33:57 +0200

    Uitwerking van de exercise

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,9 +1,44 @@
 @extends('layouts.layoutadmin')
 
 @section('topmenu')
-    
+    <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+        <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
+            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+            <path
+                d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
+            ></path>
+            </svg>
+        </div>
+        <div>
+            <p
+            class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"
+            >
+            Total clients
+            </p>
+            <p
+            class="text-lg font-semibold text-gray-700 dark:text-gray-200"
+            >
+            6389
+            </p>
+        </div>
+    </div>
 @endsection
-
+    <div class="w-full overflow-x-auto">
+        <div class="p-6 bg-white dark:bg-gray-800">
+              <table class="w-full whitespace-no-wrap">
+                <thead>
+                    <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                        <th class="px-4 py-3">Client</th>
+                        <th class="px-4 py-3">Amount</th>
+                        <th class="px-4 py-3">Status</th>
+                        <th class="px-4 py-3">Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>    
+        </div>
+    </div>
 @section('content')
 
 @endsection
